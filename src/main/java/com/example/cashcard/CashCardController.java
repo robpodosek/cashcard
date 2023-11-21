7124004c497306cf59ec9736df7d2e1cb141b8b0
robpodosek <rpodosek@gmail.com>
2023-11-20 22:43:36 -0800

    Add PathVariable

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
-
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
