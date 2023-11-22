d1ae7903e8e24be0799b5cf86407ceebcd9c4266
robpodosek <rpodosek@gmail.com>
2023-11-21 22:38:52 -0800

    Add CashCard CRUD Repository

---
+++ src/main/java/com/example/cashcard/CashCardRepository.java
@@ -0,0 +1,10 @@
+package com.example.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * CashCardRepository
+ */
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
+
+}
