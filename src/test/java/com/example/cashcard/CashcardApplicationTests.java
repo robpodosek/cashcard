1f2f2fd1757f6b927db1433ab14e1fededc805f9
robpodosek <rpodosek@gmail.com>
2023-12-15 08:56:03 -0800

    Add more robust delete tests

--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -223,4 +223,17 @@ class CashCardApplicationTests {
                                 .exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
                 assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         }
+
+        @Test
+        void shouldNotAllowDeletionOfCashCardsTheyDoNotOwn() {
+                ResponseEntity<Void> deleteResponse = restTemplate
+                                .withBasicAuth("robbie1", "abc123")
+                                .exchange("/cashcards/102", HttpMethod.DELETE, null, Void.class);
+                assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+                ResponseEntity<String> getResponse = restTemplate
+                                .withBasicAuth("marvin2", "ghi789")
+                                .getForEntity("/cashcards/102", String.class);
+                assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
 }
