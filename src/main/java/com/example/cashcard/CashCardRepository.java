be78541c10087b27ba55167f8064e64b30fd0217
robpodosek <rpodosek@gmail.com>
2023-12-15 08:51:25 -0800

    Check if the card exists before deleting

--- src/main/java/com/example/cashcard/CashCardRepository.java
+++ src/main/java/com/example/cashcard/CashCardRepository.java
@@ -12,4 +12,6 @@ interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSo
     CashCard findByIdAndOwner(Long id, String owner);
 
     Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
+
+    boolean existsByIdAndOwner(Long id, String name);
 }
--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -215,4 +215,12 @@ class CashCardApplicationTests {
                                 .getForEntity("/cashcards/99", String.class);
                 assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         }
+
+        @Test
+        void shouldNotDeleteACashCardThatDoesNotExist() {
+                ResponseEntity<Void> deleteResponse = restTemplate
+                                .withBasicAuth("robbie1", "abc123")
+                                .exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+                assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        }
 }
